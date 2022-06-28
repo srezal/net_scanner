49d04a90b3f3d33b67826fc29b916de6584ea999
srezal <sereshka0967@gmail.com>
2022-06-28 15:00:48 -0400

    update output view

--- main.py
+++ main.py
@@ -15,12 +15,12 @@ def get_arguments():
 
 
 def get_own_ip():
+    options = get_arguments()
     own_ip = re.search(r"\d+.\d+.\d+.\d+", subprocess.check_output(["ifconfig", options.interface]).decode())
     return own_ip.group(0)
 
 
 def scan(ip):
-    MacLookup().update_vendors()
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
@@ -34,22 +34,18 @@ def scan(ip):
         clients_list.append({"ip" : element[1].psrc,
                              "mac" : element[1].hwsrc,
                              "vendor" : vendor})
-
     return clients_list, [element[1].pdst for element in unanswered_list]
 
 
 def print_result(clients_list):
     for client in clients_list:
-        print(client['ip'], client['mac'], client["vendor"])
+        print(client['ip'] + "\t" + client['mac'] + "\t" + client["vendor"])
 
 
-options = get_arguments()
-own_ip = get_own_ip()
-print("IP\t\t\t\tMAC address\t\t\t\tVendor\n" + "-"*60)
-clients_list, unanswered_list = scan(f"{own_ip}/24")
-print_result(clients_list)
+print("\nIP\t\tMAC address\t\tVendor\n")
+answered_list, unanswered_list = scan(f"{get_own_ip()}/24")
+print_result(answered_list)
 while unanswered_list:
-    clients_list, unanswered_list = scan(unanswered_list)
-    print_result(clients_list)
-
+    answered_list, unanswered_list = scan(unanswered_list)
+    print_result(answered_list)
 
